bce62f66c2fe5974da9244eecb1692397abba641
Aleksi Lampinen <aleksi.lampinen@outlook.com>
2020-03-27 14:15:48 +0200

    Initial commit

---
+++ todo/app/Views/templates/header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <title><?= $title?></title>
+    
+</head>
+<body>
+    <div class="container">
